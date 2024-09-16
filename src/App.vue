7202f3fb6ea68e5448f85e2503a0e7b96d9a937e
gerry <gerrydesrian488@gmail.com>
2024-09-16 19:49:01 +0700

    navbar and footer

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import Navbar from './components/Navbar.vue'
+import Footer from './components/Footer.vue'
+
+</script>
+
+<template>
+  <div class="bg-secondary">
+    <Navbar></Navbar>
+    <div class="bg-gradient-to-t from-primary to-secondary min-h-screen">
+
+    </div>
+    <div class="bg-primary min-h-screen">
+      
+
+    </div>
+    <div class="bg-gradient-to-t from-secondary to-primary min-h-screen">
+      
+
+    </div>
+    <Footer></Footer>
+  </div>
+ 
+
+ 
+  
+</template>
+
+<style scoped>
+</style>
