f5630177483d2a12c7eb513a83494006135adf09
gerry <gerrydesrian488@gmail.com>
2024-09-18 12:47:02 +0700

    update breakpoint

--- src/components/Nutrition.vue
+++ src/components/Nutrition.vue
@@ -7,10 +7,10 @@ const totalProgress = 5000;
 </script>
 
 <template>
-  <div class="card text-neutral-content w-96 bg-gray-50 rounded-lg shadow-lg py-[-30px]">
+  <div class="card text-neutral-content w-full bg-gray-50 rounded-lg shadow-lg py-[-30px]">
     <div class="card-body items-center text-center space-y-1">
          
-      <div class="flex flex-col items-center space-x-15 w-full -mt-5 -mb-3">
+      <div class="flex flex-col items-center space-x-15 -mt-5 -mb-3">
         <p class="text-sm w-22 -mb-4">{{ name }}</p>
           <div class="divider lg:divider-vertical"></div>
         <div class="flex flex-col items-center space-x-15 w-full -mt-3">
--- src/views/Tracker.vue
+++ src/views/Tracker.vue
@@ -6,24 +6,26 @@ import Nutrition from '../components/Nutrition.vue'
 </script>
 
 <template>
-    <div class="container bg-primary mx-auto flex flex-row justify-center space-x-10 h-[80vh] mt-10">
-      <div class="card bg-neutral text-neutral-content w-[50vh]">
+    <div class="container bg-primary mx-auto flex flex-col md:flex-row justify-center space-x-0 md:space-x-10 space-y-10 md:space-y-0 mt-10">
+      <div class="card bg-neutral text-neutral-content w-full md:w-[50vh]">
         <div class="card-body items-center text-center">
-            <Switcher></Switcher>
-            <ImageTracker></ImageTracker>
+          <Switcher></Switcher>
+          <ImageTracker></ImageTracker>
         </div>
       </div>
-      <div class="card bg-neutral text-neutral-content w-[50vh] hidden md:block">
+      <div class="card bg-neutral text-neutral-content w-full md:w-[50vh]">
         <div class="card-body items-center text-center">
-            <h1 class="text-xl mb-3">Progress</h1>
-            <Nutrition name="Calorie"></Nutrition>
-            <Nutrition name="Carbohydrate"></Nutrition>
-            <Nutrition name="Sugar"></Nutrition>
-            <Nutrition name="Fat"></Nutrition>
-            <Nutrition name="Protein"></Nutrition>
+          <h1 class="text-xl mb-3">Progress</h1>
+          <Nutrition name="Calorie"></Nutrition>
+          <Nutrition name="Carbohydrate"></Nutrition>
+          <Nutrition name="Sugar"></Nutrition>
+          <Nutrition name="Fat"></Nutrition>
+          <Nutrition name="Protein"></Nutrition>
         </div>
-    </div>
+      </div>
     </div>
   </template>
+  
+  
   
   
