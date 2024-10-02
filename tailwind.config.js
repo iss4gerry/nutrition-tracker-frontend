8e2b5bf1c05de58ac77538e9ac2c44cc8affd13f
gerry <gerrydesrian488@gmail.com>
2024-10-02 21:19:01 +0700

    update remove dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,68 +1,67 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Montserrat', 'sans-serif'],
-            },
-        },
-    },
-    daisyui: {
-        themes: [
-            {
-                mytheme: {
-                    primary: '#C9CCD5',
-                    secondary: '#E4D8DC',
-                    accent: '#45474B',
-                    neutral: '#C8ACD6',
-                    'base-100': '#ffffff',
-                },
-            },
-            'light',
-            'dark',
-            'cupcake',
-            'bumblebee',
-            'emerald',
-            'corporate',
-            'synthwave',
-            'retro',
-            'cyberpunk',
-            'valentine',
-            'halloween',
-            'garden',
-            'forest',
-            'aqua',
-            'lofi',
-            'pastel',
-            'fantasy',
-            'wireframe',
-            'black',
-            'luxury',
-            'dracula',
-            'cmyk',
-            'autumn',
-            'business',
-            'acid',
-            'lemonade',
-            'night',
-            'coffee',
-            'winter',
-            'dim',
-            'nord',
-            'sunset',
-        ], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
-        darkTheme: 'dark', // name of one of the included themes for dark mode
-        base: true, // applies background color and foreground color for root element by default
-        styled: true, // include daisyUI colors and design decisions for all components
-        utils: true, // adds responsive and modifier utility classes
-        prefix: '', // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
-        logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
-        themeRoot: ':root', // The element that receives theme color CSS variables
-    },
-    plugins: [
-        require('daisyui'),
-        require('tailwindcss'),
-        require('autoprefixer'),
-    ],
+	content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ['Montserrat', 'sans-serif'],
+			},
+		},
+	},
+	daisyui: {
+		themes: [
+			{
+				mytheme: {
+					primary: '#C9CCD5',
+					secondary: '#E4D8DC',
+					accent: '#45474B',
+					neutral: '#C8ACD6',
+					'base-100': '#ffffff',
+				},
+			},
+			'light',
+			'dark',
+			'cupcake',
+			'bumblebee',
+			'emerald',
+			'corporate',
+			'synthwave',
+			'retro',
+			'cyberpunk',
+			'valentine',
+			'halloween',
+			'garden',
+			'forest',
+			'aqua',
+			'lofi',
+			'pastel',
+			'fantasy',
+			'wireframe',
+			'black',
+			'luxury',
+			'dracula',
+			'cmyk',
+			'autumn',
+			'business',
+			'acid',
+			'lemonade',
+			'night',
+			'coffee',
+			'winter',
+			'dim',
+			'nord',
+			'sunset',
+		], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]// name of one of the included themes for dark mode
+		base: true, // applies background color and foreground color for root element by default
+		styled: true, // include daisyUI colors and design decisions for all components
+		utils: true, // adds responsive and modifier utility classes
+		prefix: '', // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
+		logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
+		themeRoot: ':root', // The element that receives theme color CSS variables
+	},
+	plugins: [
+		require('daisyui'),
+		require('tailwindcss'),
+		require('autoprefixer'),
+	],
 };
